f5b5aff2c0aed0fd90573e3d03eec08258850fbf
K-Sato1995 <Katsuki_Sato0130@icloud.com>
2024-01-22 21:17:51 +0900

    feat: embed code into tempatel

---
+++ contents/test.md
@@ -0,0 +1,11 @@
+# Heading1
+
+description1
+
+## Heading2
+
+description1
+
+- list
+ - item
+ - item
--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
-
-	"io/ioutil"
-	"log"
 )
+
+type Template struct {
+	HTMLTitle       string
+	MetaDescription string
+	PageTitle       string
+	Content         template.HTML
+}
 
 func mdToHTML(md []byte) []byte {
 	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
@@ -16,17 +28,52 @@ func mdToHTML(md []byte) []byte {
 	htmlFlags := html.CommonFlags | html.HrefTargetBlank
 	opts := html.RendererOptions{Flags: htmlFlags}
 	renderer := html.NewRenderer(opts)
-
 	return markdown.Render(doc, renderer)
 }
 
 func main() {
-	content, err := ioutil.ReadFile("./contents/testMd.md")
+	// content, err := ioutil.ReadFile("./contents/testMd.md")
+	// if err != nil {
+	// 	log.Fatalf("Error reading Markdown file: %v", err)
+	// }
+	// md := []byte(content)
+	// html := mdToHTML(md)
+	// fmt.Println(string(html))
+	htmlTmpl, err := template.ParseGlob("templates/detail.html")
 	if err != nil {
-		log.Fatalf("Error reading Markdown file: %v", err)
+		log.Fatal(err)
 	}
+	filepath.Walk("contents", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Println(err)
+			return nil
+		}
 
-	md := []byte(content)
-	html := mdToHTML(md)
-	println(string(html))
+		if !info.IsDir() && filepath.Ext(path) == ".md" {
+			content, err := ioutil.ReadFile(path)
+			if err != nil {
+				log.Println(err)
+				return nil
+			}
+			data := Template{
+				HTMLTitle:       "Example Title",
+				MetaDescription: "Example Description",
+				PageTitle:       "My Page Title",
+				Content:         template.HTML(content),
+			}
+
+			var renderedContent bytes.Buffer
+			err = htmlTmpl.Execute(&renderedContent, data)
+			if err != nil {
+				log.Println("Error executing template:", err)
+				return nil
+			}
+			fmt.Println(renderedContent.String())
+		}
+		return nil
+	})
+}
+
+func fileWalk() {
+
 }
